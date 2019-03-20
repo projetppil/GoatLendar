f6110bbaabd4fe97573f908c5468d970fa8228a4
karim213 <kabdi213@gmail.com>
2019-03-20 17:54:16 +0100

    Menu

--- app/src/main/java/com/example/dell/goatlendar/ControleurMenu.java
+++ app/src/main/java/com/example/dell/goatlendar/ControleurMenu.java
@@ -3,12 +3,21 @@ package com.example.dell.goatlendar;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ControleurMenu extends Application {
+public class ControleurMenu extends AppCompatActivity {
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout dl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,15 +32,15 @@ public class ControleurMenu extends Application {
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24px);
 
         dl = findViewById(R.id.ac);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this , dl ,R.string.Open , R.string.Close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
 
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         dl.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-        NavigationView barre_de_nav = (NavigationView)findViewById(R.id.barre_de_nav);
+        NavigationView barre_de_nav = (NavigationView) findViewById(R.id.barre_de_nav);
         barre_de_nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected( MenuItem menuItem) {
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
                 FragmentManager fg = getSupportFragmentManager();
                 switch (menuItem.getItemId()) {
                     case R.id.notification:
@@ -40,7 +49,7 @@ public class ControleurMenu extends Application {
                     case R.id.calendrier:
                         ControleurAccueil app = new ControleurAccueil();
                         toolbar.setTitle("Événements");
-                        fg.beginTransaction().replace(R.id.main , app).commit();
+                        fg.beginTransaction().replace(R.id.main, app).commit();
 
                         return true;
                     case R.id.calendrierEvent:
@@ -58,7 +67,7 @@ public class ControleurMenu extends Application {
                     case R.id.settings:
                         ControleurParametres parametres = new ControleurParametres();
                         toolbar.setTitle("Paramètres de compte");
-                        fg.beginTransaction().replace(R.id.main , parametres).addToBackStack("back").commit();
+                        fg.beginTransaction().replace(R.id.main, parametres).addToBackStack("back").commit();
 
                         return true;
                     case R.id.exit:
@@ -71,8 +80,35 @@ public class ControleurMenu extends Application {
         });
         ControleurAccueil app = new ControleurAccueil();
         FragmentManager fg = getSupportFragmentManager();
-        fg.beginTransaction().replace(R.id.main , app).commit();
+        fg.beginTransaction().replace(R.id.main, app).commit();
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_droit, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                dl.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.item1:
+                //ajouter l'action d'ouverture d'activity notification
+                Toast.makeText(getApplicationContext(), "item1", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                //ajouter l'action d'ouverture d'activity exit
+                Toast.makeText(getApplicationContext(), "item2", Toast.LENGTH_LONG).show();
+                return true;
+        }
+
+         return false;
+    }
+
 }
